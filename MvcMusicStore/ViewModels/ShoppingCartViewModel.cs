f579c8e9e10f4a267f8d84447483d38136e6f56f
tranphuoctuan04 <tranphuoctuan04@gmail.com>
2015-01-06 09:42:57 +0700

    sửa cả local và client

--- MvcMusicStore/ViewModels/ShoppingCartViewModel.cs
+++ MvcMusicStore/ViewModels/ShoppingCartViewModel.cs
@@ -3,6 +3,7 @@ using MvcMusicStore.Models;
 
 namespace MvcMusicStore.ViewModels
 {
+    //nội dung sửa lại ở local
     public class ShoppingCartViewModel
     {
         public List<Cart> CartItems { get; set; }
